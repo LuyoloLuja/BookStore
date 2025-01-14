a6a5774119073209e2656c261c6336988e030fcf
LuyoloLuja <luyolo.mntuyedwa97@gmail.com>
2025-01-14 22:55:51 +0200

    added db

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+using BookStore.Models.Domain;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
--- obj/Debug/net8.0/BookStore.AssemblyInfo.cs
+++ obj/Debug/net8.0/BookStore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05e12ed86242ed45a780abd6deb1a7b23469e318")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9eb9e3ee4bcad2618f613cfb6c2d0a50fb85fea5")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
